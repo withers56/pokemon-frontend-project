96c2170e94cf24dfedb2c4225bc6ee6cd20d4442
withers56 <william.k.withers@gmail.com>
2022-07-05 14:09:21 -0500

    practice storing info into localstorage, pokapi wants you to store information inot localstorage to limit calls

--- pokemon-app/src/pages/AllPokemon.js
+++ pokemon-app/src/pages/AllPokemon.js
@@ -2,7 +2,8 @@ function AllPokemon() {
     function getPokis() {
         fetch('https://pokeapi.co/api/v2/pokemon/1').then((resp) => resp.json())
             .then((data) => {
-                console.log(data);
+                window.localStorage.setItem(data.name, JSON.stringify(data));
+                console.log(JSON.parse(window.localStorage.getItem(data.name)))
             })
     }
     return (
